8a7c04589626033f94494d66c4eff01b9dfebac8
Michael Fuller <ful422.m@gmail.com>
2024-04-25 15:24:17 -0500

    dots: add get_aur abbr

--- fish/conf.d/ful09003_config.fish
+++ fish/conf.d/ful09003_config.fish
@@ -1,3 +1,19 @@
 echo -n "Setting abbrs (ful09003_config).."
 abbr ga 'git add'
 abbr mtrr 'mtr -nrc 10'
+abbr aurs 'cd ~/Documents/development/aurs'
+
+function get_aur
+  set AURPATH_BASE $HOME/Documents/development/aurs
+  set AURPATH_DESIRED $AURPATH_BASE/$argv
+
+  if test -d $AURPATH_BASE/$argv
+     cd $AURPATH_DESIRED
+     echo "updating AUR in $(pwd)..."
+     git pull
+     prevd
+  else
+    echo "fetching new AUR..."
+    git clone https://aur.archlinux.org/$argv.git    
+  end
+end
