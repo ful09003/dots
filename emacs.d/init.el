e4d07d30e0972f833fa8440f5d1b8eba9060db37
Michael Fuller <ful422.m@gmail.com>
2024-06-23 15:58:52 -0500

    port existing init.el into dots; has a small bit already via https://systemcrafters.net/emacs-from-scratch/basics-of-emacs-configuration/ and mastering emacs/nov.el

---
+++ emacs.d/init.el
@@ -0,0 +1,54 @@
+;; Do not show splash
+
+(setq inhibit-startup-message t)
+
+;; Display line numbers in every buffer
+(global-display-line-numbers-mode 1)
+
+;; theme
+(load-theme 'modus-vivendi t)
+
+;; font
+(add-to-list 'default-frame-alist
+             '(font . "Maple Mono SC NF-12"))
+
+;; startup-y things. Mostly ripped from emacs-bedrock
+(setopt display-time-default-load-average nil)
+(setopt sentence-end-double-space nil)
+(when (display-graphic-p)
+  (context-menu-mode))
+;; Don't litter file system with *~ backup files; put them all inside
+;; ~/.emacs.d/backup or wherever
+(defun bedrock--backup-file-name (fpath)
+  "Return a new file path of a given file path.
+If the new path's directories does not exist, create them."
+  (let* ((backupRootDir "~/.emacs.d/emacs-backup/")
+         (filePath (replace-regexp-in-string "[A-Za-z]:" "" fpath )) ; remove Windows driver letter in path
+         (backupFilePath (replace-regexp-in-string "//" "/" (concat backupRootDir filePath "~") )))
+    (make-directory (file-name-directory backupFilePath) (file-name-directory backupFilePath))
+    backupFilePath))
+(setopt make-backup-file-name-function 'bedrock--backup-file-name)
+
+;; Show the help buffer after startup
+(add-hook 'after-init-hook 'help-quick)
+
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
+;; and `package-pinned-packages`. Most users will not need or want to do this.
+;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(package-initialize)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(nov)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(add-to-list 'auto-mode-alist '("\\.epub\\'" . nov-mode))
