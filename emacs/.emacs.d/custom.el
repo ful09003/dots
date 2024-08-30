29a2f296104f6a2595427f7cad85dea9eff8e0ef
Michael Fuller <ful422.m+github@gmail.com>
2024-08-30 20:24:43 +0000

    Move customizations to new file. Ignore blatant error about multiple defs, and enable some things that I like

---
+++ emacs/.emacs.d/custom.el
@@ -0,0 +1,24 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(backup-directory-alist '(("." . "/home/michael/Documents/backups")))
+ '(column-number-mode t)
+ '(delete-old-versions t)
+ '(dired-free-space 'first)
+ '(display-line-numbers 'relative)
+ '(display-line-numbers-major-tick 10)
+ '(display-line-numbers-minor-tick 5)
+ '(global-hl-line-mode t)
+ '(inhibit-startup-screen t)
+ '(package-selected-packages '(magit nov.el nov))
+ '(require-final-newline t)
+ '(scroll-bar-mode nil)
+ '(show-trailing-whitespace t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
