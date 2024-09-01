7c99e0c3d7be4acd0a148032952b325601bf0fe0
Michael Fuller <ful422.m+github@gmail.com>
2024-09-01 15:51:53 +0000

    remove empty customizations, move custom-file, attempt to fix broken add-hook for lsp->go

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -5,18 +5,6 @@
 ;; and `package-pinned-packages`. Most users will not need or want to do this.
 ;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 (package-initialize)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages '(magit nov.el nov)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
 
 ;; Use ibuffer
 (global-set-key [remap list-buffers] 'ibuffer)
@@ -24,18 +12,21 @@
 (global-set-key (kbd "M-o") 'other-window)
 
 
-;; https://www.gnu.org/software/emacs/manual/html_node/emacs/Saving-Customizations.html
-(setq custom-file (concat user-emacs-directory "custom.el"))
-(load custom-file 'noerror)
-
 ;; File type associations w/ mode
 ;; https://depp.brause.cc/nov.el/
 (add-to-list 'auto-mode-alist '("\\.epub\\'" . nov-mode))
 
 ;; https://emacs-lsp.github.io/lsp-mode/page/installation/
 (require 'lsp-mode)
-(add-hook 'go-mode-hook' #'lsp-deffered)
+(add-hook 'go-mode-hook #'lsp-deferred)
+
+
 (defun lsp-go-install-save-hooks ()
   (add-hook 'before-save-hook #'lsp-format-buffer t t)
   (add-hook 'before-save-hook #'lsp-organize-imports t t))
 (add-hook 'go-mode-hook #'lsp-go-install-save-hooks)
+
+
+;; https://www.gnu.org/software/emacs/manual/html_node/emacs/Saving-Customizations.html
+(setq custom-file (concat user-emacs-directory "custom.el"))
+(load custom-file 'noerror)
