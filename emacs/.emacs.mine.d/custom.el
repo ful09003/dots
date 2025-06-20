7fd063b9ab6885441dcb15cf2df975561ed304ca
Michael Fuller <ful422.m+github@gmail.com>
2025-06-20 16:07:20 -0500

    ope

---
+++ emacs/.emacs.mine.d/custom.el
@@ -0,0 +1,37 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(backup-directory-alist '(("." . "/home/michael/Documents/backups")))
+ '(column-number-mode t)
+ '(custom-enabled-themes '(ope))
+ '(custom-safe-themes
+   '("243bee18ca995f9f229c134f1fdae6c41cff5d8628cf1a875e6fffe41143834a" "46410d91e761e9ebc0e3c4255b7fe0c3d3c9ffa78ed2018be5e4799b6bd8b9c7" "08236520e4574ce6ce9560e8920bae3a6f5b838ec9b499c583c67230c2f69259" default))
+ '(default-frame-alist '((undecordated . t)))
+ '(delete-old-versions t)
+ '(display-fill-column-indicator t)
+ '(display-line-numbers t)
+ '(display-line-numbers-major-tick 15)
+ '(display-time-24hr-format t)
+ '(display-time-day-and-date t)
+ '(fill-column 80)
+ '(global-display-fill-column-indicator-mode t)
+ '(global-hl-line-mode t)
+ '(global-subword-mode t)
+ '(global-visual-line-mode t)
+ '(inhibit-startup-screen t)
+ '(initial-scratch-message
+   ";; This buffer is for text that is not saved, and for Lisp evaluation.\12;; To create a file, visit it with \\[find-file] and enter text in its buffer.\12\12Now go slay some dragons, get that bread, whatever you need you ABSOLUTE BADDIE\12")
+ '(package-selected-packages
+   '(magit trashed dired-subtree corfu orderless marginalia vertico nerd-icons-dired nerd-icons-corfu nerd-icons-completion nerd-icons))
+ '(shell-kill-buffer-on-exit t)
+ '(show-trailing-whitespace t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t nil)))
+ '(mode-line ((t nil)))
+ '(trailing-whitespace ((t (:background "tomato")))))
---
+++ emacs/.emacs.mine.d/init.el
@@ -0,0 +1,151 @@
+;; Starting here, using https://protesilaos.com/codelog/2024-11-28-basic-emacs-configuration/
+(setq custom-file (locate-user-emacs-file "custom.el"))
+(load custom-file :no-error-if-file-is-missing)
+
+(require 'package)
+(package-initialize)
+
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+
+(when (< emacs-major-version 29)
+  (unless (package-installed-p 'use-package)
+    (unless package-archive-contents
+      (package-refresh-contents))
+    (package-install 'use-package)))
+
+(add-to-list 'display-buffer-alist
+	     '("\\`\\*\\(Warnings\\|Compile-Log\\)\\*\\'"
+	       (display-buffer-no-window)
+	       (allow-no-window . t)))
+
+(use-package delsel
+  :ensure nil ; built-in
+  :hook (after-init . delete-selection-mode))
+
+(defun prot/keyboard-quit-dwim ()
+  "Do-What-I-Mean behaviour for a general 'keyboard-quit'.
+
+The generic 'keyboard-quit' does not do the expected thing when the minibuffer is open.
+We want it to close the minibuffer even if not focused.
+
+DWIM means:
+
+- When the region is active, disable it
+- When a minibuffer is open but not focused, close it
+- When the Completions buffer is selected, close it.
+- In every other case use regular 'keyboard-quit'."
+  (interactive)
+  (cond
+   ((region-active-p)
+    (keyboard-quit))
+   ((derived-mode-p 'completion-list-mode)
+    (delete-completion-window))
+   ((> (minibuffer-depth) 0)
+    (abort-recursive-edit))
+   (t
+    (keyboard-quit))))
+
+(define-key global-map (kbd "C-g") #'prot/keyboard-quit-dwim)
+
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
+
+(use-package nerd-icons
+  :ensure t)
+
+(use-package nerd-icons-completion
+  :ensure t
+  :after marginalia
+  :config
+  (add-hook 'marginalia-mode-hook #'nerd-icons-completion-marginalia-setup))
+
+(use-package nerd-icons-corfu
+  :ensure t
+  :after corfu
+  :config
+  (add-to-list 'corfu-margin-formatters #'nerd-icons-corfu-formatter))
+
+(use-package nerd-icons-dired
+  :ensure t
+  :hook
+  (dired-mode . nerd-icons-dired-mode))
+
+(use-package vertico
+  :ensure t
+  :hook (after-init . vertico-mode))
+
+(use-package marginalia
+  :ensure t
+  :hook (after-init . marginalia-mode))
+
+(use-package savehist
+  :ensure nil ; built-in
+  :hook (after-init . savehist-mode))
+
+(use-package orderless
+  :ensure t
+  :config
+  (setq completion-styles '(orderless basic))
+  (setq completion-category-defaults nil)
+  (setq completion-category-overrides nil))
+
+(use-package corfu
+  :ensure t
+  :hook (after-init . global-corfu-mode)
+  :bind (:map corfu-map ("<tab>" . corfu-complete))
+  :config
+  (setq tab-always-indent 'complete)
+  (setq corfu-preview-current nil)
+  (setq corfu-min-width 20)
+
+  (setq corfu-popupinfo-delay '(1.24 . 0.5))
+  (corfu-popupinfo-mode 1) ; shows documentation after 'corfu-popupinfo-delay'
+
+  ;; sort by input history
+  (with-eval-after-load 'savehist
+    (corfu-history-mode 1)
+    (add-to-list 'savehist-additional-variables 'corfu-history)))
+
+(use-package dired
+  :ensure nil ; default
+  :commands (dired)
+  :hook
+  ((dired-mode . hl-line-mode))
+  :config
+  (setq dired-recursive-copies 'always)
+  (setq dired-recursive-deletes 'always)
+  (setq delete-by-moving-to-trash t)
+  (setq dired-dwim-target t))
+
+(use-package dired-subtree
+  :ensure t
+  :after dired
+  :bind
+  ( :map dired-mode-map
+    ("<tab>" . dired-subtree-toggle)
+    ("TAB" . dired-subtree-toggle)
+    ("<backtab>" . dired-subtree-remove)
+    ("S-TAB" . dired-subtree-remove))
+  :config
+  (setq dired-subtree-use-backgrounds nil))
+
+(use-package trashed
+  :ensure t
+  :commands (trashed)
+  :config
+  (setq trashed-action-confirmer 'y-or-n-p)
+  (setq trashed-use-header-line t)
+  (setq trashed-sort-key '("Date deleted" . t))
+  (setq trashed-date-format "%Y-%m-%d %H:%M:%S"))
+
+;; my own configs, starting here
+;; rebind other-window (C-x o) to M-o for convenience
+(global-set-key (kbd "M-o") 'other-window)
+;; rebind C-x C-b to buffer-menu so that I can select buffer in current window
+(global-set-key (kbd "C-x C-b") 'buffer-menu)
+;; install magit
+(use-package magit
+  :ensure t
+  :bind
+  (("C-x g" . magit-status)))
