22ece72439f1402db740d5a0ba2e9c098efb5a77
Michael Fuller <ful422.m+github@gmail.com>
2026-03-20 17:49:56 -0500

    custom.el: updates
    Random drift fixup w/ VCS?

--- emacs/.emacs.mine.d/custom.el
+++ emacs/.emacs.mine.d/custom.el
@@ -7,7 +7,8 @@
  '(column-number-mode t)
  '(custom-enabled-themes '(ope))
  '(custom-safe-themes
-   '("06c09441e9da91ea7a473e6d42f9b0966632a345cf5dac6c3136d6ba66b188e5"
+   '("e900ae738225380abd1edc0c76535a12b8a6669c7a3180431ba0157a47bbf75e"
+     "06c09441e9da91ea7a473e6d42f9b0966632a345cf5dac6c3136d6ba66b188e5"
      "40e54617ed0a2cca397d7e4516122e1f0c4e0a6b1e8faa74d8cd7c1760ebdd1d"
      "243bee18ca995f9f229c134f1fdae6c41cff5d8628cf1a875e6fffe41143834a"
      "46410d91e761e9ebc0e3c4255b7fe0c3d3c9ffa78ed2018be5e4799b6bd8b9c7"
@@ -27,10 +28,7 @@
  '(inhibit-startup-screen t)
  '(initial-scratch-message
    ";; This buffer is for text that is not saved, and for Lisp evaluation.\12;; To create a file, visit it with \\[find-file] and enter text in its buffer.\12\12Now go slay some dragons, get that bread, whatever you need you ABSOLUTE BADDIE\12")
- '(package-selected-packages
-   '(catppuccin-theme corfu dired-subtree ellama hl-todo magit marginalia
-		      nerd-icons-completion nerd-icons-corfu nerd-icons-dired
-		      nov orderless org-modern standard-themes trashed vertico))
+ '(package-selected-packages nil)
  '(shell-kill-buffer-on-exit t)
  '(show-trailing-whitespace t))
 (custom-set-faces
