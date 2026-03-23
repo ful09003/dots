0e49abcdefc67c902553564e869902761e5ba0f2
Michael Fuller <ful422.m+github@gmail.com>
2026-03-23 15:47:02 -0500

    fix (media): Format, some more stations
    Add KEXP Seattle and 313.FM Detroit

--- emacs/.emacs.mine.d/concerns/custom-media.el
+++ emacs/.emacs.mine.d/concerns/custom-media.el
@@ -11,10 +11,11 @@
   (empv-radio-channels
 	  '(("Eden Radio" . "https://www.edenofthewest.com/public/eden-radio/playlist.pls")
 	    ("SomaFM - Groove Salad" . "http://www.somafm.com/groovesalad.pls")
- ("SomaFM - Drone Zone" . "http://www.somafm.com/dronezone.pls")
- ("SomaFM - Sonic Universe" . "https://somafm.com/sonicuniverse.pls")
- ("SomaFM - Metal" . "https://somafm.com/metal.pls")
- ("SomaFM - Vaporwaves" . "https://somafm.com/vaporwaves.pls")
-	    )))
+	    ("SomaFM - Drone Zone" . "http://www.somafm.com/dronezone.pls")
+	    ("SomaFM - Sonic Universe" . "https://somafm.com/sonicuniverse.pls")
+	    ("SomaFM - Metal" . "https://somafm.com/metal.pls")
+	    ("SomaFM - Vaporwaves" . "https://somafm.com/vaporwaves.pls")
+	    ("KEXP - Seattle" . "https://kexp.streamguys1.com/kexp160.aac")
+	    ("313.FM - Detroit" . "http://icecast.ofdoom.com:8000/burst.mp3"))))
 
 (provide 'custom-media)
